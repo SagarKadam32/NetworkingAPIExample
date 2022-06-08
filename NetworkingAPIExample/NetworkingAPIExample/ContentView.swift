14097ccfb41c989cd16405b53b48762631d7ab46
sagar kadam <misc.sagarkadam@gmail.com>
2022-06-08 11:19:16 +0530

    Code Update : Initial Commit

---
+++ NetworkingAPIExample/NetworkingAPIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkingAPIExample
+//
+//  Created by Sagar Kadam on 08/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkingAPIExample/NetworkingAPIExample/NetworkingAPIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAPIExampleApp.swift
+//  NetworkingAPIExample
+//
+//  Created by Sagar Kadam on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAPIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
